21aafdd08f988dbab025a9599675e3631524fd96
Aborigenius <spiridonovivan@gmail.com>
2017-10-10 16:48:22 +0200

    More, MORe

---
+++ Programing Fundamentals/Methods/Cube Properties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cubeSide = double.Parse(Console.ReadLine());
+            string parameter = Console.ReadLine();
+
+            CalcCube(cubeSide, parameter);
+        }
+
+        static void CalcCube(double cubeSide, string parameter)
+        {
+            double result = 0;
+            if (parameter == "face")
+            {//
+                result = Math.Sqrt(2 * (cubeSide * cubeSide));
+            }
+            if (parameter == "space")
+            {
+                result = Math.Sqrt(3 * (cubeSide * cubeSide));
+            }
+            if (parameter == "volume")
+            {
+                result = Math.Pow(cubeSide, 3);
+
+            }
+            if (parameter == "area")
+            {
+                result = 6 * (Math.Pow(cubeSide, 2));
+            }
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
---
+++ Programing Fundamentals/Methods/Geometry Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine().ToLower();
+            double result = 0;
+            if (figureType == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                result = (side * height) / 2;
+            }
+            if (figureType == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                result = side * side;
+            }
+            if (figureType == "rectangle")
+            {
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                result = height * width;
+            }
+            if (figureType == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                result = radius * radius * Math.PI;
+            }
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
