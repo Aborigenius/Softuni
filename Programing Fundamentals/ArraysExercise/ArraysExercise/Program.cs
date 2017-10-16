a060171df98cac38df01fcd223ae67805dc06682
Aborigenius <spiridonovivan@gmail.com>
2017-10-16 16:28:31 +0200

    Arrays Ex

---
+++ Programing Fundamentals/ArraysExercise/ArraysExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr1 = Console.ReadLine().Split(' ').ToArray();
+
+            string[] arr2 = Console.ReadLine().Split(' ').ToArray();
+
+
+        }
+    }
+}
---
+++ Programing Fundamentals/ArraysExercise/ReverseArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            string[] reversed = new string[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed[input.Length - i - 1] = input[i];
+            }
+            //or use reverse which does pretty much the same, just 
+            //needs .ToArray() at the end because reverse use IEnumerable
+            string[] reversed2 = input.Reverse().ToArray();
+
+            Console.WriteLine(string.Join(" <=> ", reversed));
+            // or we go balistic and do
+
+            Console.WriteLine(String.Join(" ", Console.ReadLine().Split(' ').Reverse().ToArray()));
+        }
+    }
+}
