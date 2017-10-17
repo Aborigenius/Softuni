2f9a27a2f46f15b762283f78bfb315f2fd420f72
Aborigenius <spiridonovivan@gmail.com>
2017-10-17 19:23:39 +0200

    Task 1 Solved

--- Programing Fundamentals/ArraysExercise/ArraysExercise/Program.cs
+++ Programing Fundamentals/ArraysExercise/ArraysExercise/Program.cs
@@ -11,10 +11,72 @@ namespace ArraysExercise
         static void Main(string[] args)
         {
 
-            string[] arr1 = Console.ReadLine().Split(' ').ToArray();
-
-            string[] arr2 = Console.ReadLine().Split(' ').ToArray();
-
+            string[] arr1 = Console.ReadLine().Split(' ');
+            string[] arr2 = Console.ReadLine().Split(' ');
+            int counter = 0;
+        
+            if (arr1.Length > arr2.Length)
+            {
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    if (arr1[i] == arr2[i])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (arr1[i] == arr2[i])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            if (counter == 0)
+            {
+                Array.Reverse(arr1);
+                Array.Reverse(arr2);
+                if (arr1.Length > arr2.Length)
+                {
+                    for (int i = 0; i < arr2.Length; i++)
+                    {
+                        if (arr1[i] == arr2[i])
+                        {
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < arr1.Length; i++)
+                    {
+                        if (arr1[i] == arr2[i])
+                        {
+                            counter++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+                Console.WriteLine(counter);
 
         }
     }
