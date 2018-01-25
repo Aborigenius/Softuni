0b110b9e2312c373902d121a1b1f2c1c6241ac3f
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2018-01-25 15:24:36 +0100

    Problem 5. Calculate Sequence with Queue
    Solved

---
+++ C# Advanced/StacksAndQueuesEx/Calculate Sequence With Queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Sequence_With_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+
+            Queue<long> sequenceQueue = new Queue<long>();
+            Queue<long> helpSequence = new Queue<long>();
+            Console.Write(N + " ");
+
+            while (helpSequence.Count < 49)
+            {
+                sequenceQueue.Enqueue(N + 1);
+                sequenceQueue.Enqueue((2 * N) + 1);
+                sequenceQueue.Enqueue(N + 2);
+                N = sequenceQueue.Dequeue();
+                helpSequence.Enqueue(N);
+   
+            }
+            PrlongValues(helpSequence);
+        }
+
+        public static void PrlongValues(IEnumerable myCollection)
+        {
+            foreach (var item in myCollection)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
