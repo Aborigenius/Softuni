05bec63efcc5dd29da3069cc13338462c1bca229
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2018-01-25 15:57:37 +0100

    Truck Tour
    Solved

---
+++ C# Advanced/StacksAndQueuesEx/Truck Tour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pumpsNum = int.Parse(Console.ReadLine());
+            Queue<int[]> queue = new Queue<int[]>();
+
+            for (int i = 0; i < pumpsNum; i++)
+            {
+                int[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                queue.Enqueue(input);
+            }
+            for (int start = 0; start < pumpsNum - 1; start++)
+            {
+                int fuel = 0;
+                bool isFuelEnough = true;
+
+                for (int pumpsPassed = 0; pumpsPassed < pumpsNum; pumpsPassed++)
+                {
+                    var currentPump = queue.Dequeue();
+                    int pumpFuel = currentPump[0];
+                    int nextPumpDistance = currentPump[1];
+
+                    queue.Enqueue(currentPump);
+
+                    fuel += pumpFuel - nextPumpDistance;
+
+                    if (fuel < 0)
+                    {
+                        start += pumpsPassed;
+                        isFuelEnough = false;
+                        break;
+                    }
+                }
+                if (isFuelEnough == true)
+                {
+                    Console.WriteLine(start);
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
