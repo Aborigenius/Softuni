dbc5827c1b59243ecd9c436db5cd157bdd974502
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2018-01-30 17:01:09 +0100

    MultiDimensional Arrays
    Plus last from previous exams

---
+++ C# Advanced/StacksAndQueuesEx/Poisonous Plants/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poisonous_Plants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfPlants = int.Parse(Console.ReadLine());
+            int[] plantsPoison = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Take(NumberOfPlants).ToArray();
+            var daysCounter = new int[NumberOfPlants];
+            Stack<int> plants = new Stack<int>();
+     
+            plants.Push(0);
+            for (int i = 1; i < plantsPoison.Length; i++)
+            {
+                int maxDays = 0;
+
+                while (plants.Count > 0 && plantsPoison[plants.Peek()] >= plantsPoison[i])
+                {
+                    maxDays = Math.Max(maxDays, daysCounter[plants.Pop()]);
+                }
+
+                if (plants.Count > 0)
+                {
+                    daysCounter[i] = maxDays + 1;
+                }
+
+                plants.Push(i);
+            }
+
+            Console.WriteLine(daysCounter.Max());
+        }
+    }
+}
--- C# Advanced/StacksAndQueuesEx/Simple Text Editor/Program.cs
+++ C# Advanced/StacksAndQueuesEx/Simple Text Editor/Program.cs
@@ -11,10 +11,56 @@ namespace Simple_Text_Editor
         static void Main(string[] args)
         {
             var input = int.Parse(Console.ReadLine());
+            var text = new StringBuilder();
+            Stack<string> rememberStack = new Stack<string>();
 
             for (int i = 0; i < input; i++)
             {
+                var commandsInput = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int action = int.Parse(commandsInput[0]);
 
+                switch (action)
+                {
+                    case 1:
+                        if (commandsInput.Length > 1)
+                        {
+                            rememberStack.Push(text.ToString());
+                            text.Append(commandsInput[1]);
+                        }
+                        break;
+                    case 2:
+                        if (commandsInput.Length > 1)
+                        {
+                            var removeCount = int.Parse(commandsInput[1]);
+
+                             rememberStack.Push(text.ToString());
+                            
+                            if (removeCount > text.Length)
+                            {
+                                text.Clear();
+                                break;
+                            }
+                            text.Remove(text.Length - removeCount, removeCount);
+                        }
+                        break;
+                    case 3:
+                        if (commandsInput.Length > 1)
+                        {
+                            var indexOf = int.Parse(commandsInput[1]);
+                            if (indexOf <= text.Length && indexOf > 0)
+                            {
+                                Console.WriteLine(text[indexOf - 1]);
+                            }
+                        }
+                        break;
+                    case 4:
+                        text.Clear();
+                        text.Append(rememberStack.Pop());
+                        break;
+                    default:
+                        break;
+
+                }
             }
         }
     }
