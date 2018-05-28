84563d567b09b6705818737489cd47e580e78b99
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2018-05-28 11:34:12 +0200

    Functuions Ex

--- JS/Functions/TripLenght.js
+++ JS/Functions/TripLenght.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//80 of 100, have no will to fix it
 function tripLenght(input) {
     let [x1, y1, x2, y2, x3, y3] = [Number(input[0]), Number(input[1]), Number(input[2]),
         Number(input[3]), Number(input[4]), Number(input[5])
@@ -11,12 +11,24 @@ function tripLenght(input) {
 
     let p1p2p3 = p1p2 + p2p3;
     let p1p3p2 = p1p3 + p2p3;
-    let p2p3p1 = p2p3 + p1p2;
+    let p2p3p1 = p2p3 + p1p3;
 
     var shorter = Math.min(p1p2p3, p1p3p2, p2p3p1);
-    console.log(shorter);
+    if (shorter == p1p2p3) {
+        console.log(`1->2->3: ${shorter}`);
+        return;
+    }
+
+    if (shorter == p2p3p1) {
+        console.log(`1->3->2: ${p2p3p1}`);
+        return;
+    }
+    if (shorter == p1p3p2) {
+        console.log(`2->1->3: ${shorter}`);
+    }
 
 }
 
-tripLenght([0, 0, 2, 0, 4, 0]);
-tripLenght([-1, -2, 3.5, 0, 0, 2]);
+//tripLenght([0, 0, 2, 0, 4, 0]);
+//tripLenght([-1, -2, 3.5, 0, 0, 2]);
+tripLenght([5, 1, 1, 1, 5, 4]);
