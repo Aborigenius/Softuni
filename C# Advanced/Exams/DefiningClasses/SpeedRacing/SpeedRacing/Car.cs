659470e77bc287df211250afa297319e9a0703de
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2018-10-24 11:05:03 +0200

    More OOP exams

---
+++ C# Advanced/Exams/DefiningClasses/SpeedRacing/SpeedRacing/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    class Car
+    {
+        //Fields
+        private string model;
+        private double fuelAmmount;
+        private double fuelConsumptionPer1KM;
+        private double traveledDistance;
+
+        //Constructors
+        public Car(string model, double fuelAmmount, double fuelConsumptionPer1KM)
+        {
+            this.Model = model;
+            this.FuelAmmount = fuelAmmount;
+            this.FuelConsumptioPer1KM = fuelConsumptionPer1KM;
+            this.TraveledDistance = 0;
+        }
+
+        //Properties
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public double FuelAmmount
+        {
+            get { return this.fuelAmmount; }
+            set { this.fuelAmmount = value; }
+        }
+
+        public double FuelConsumptioPer1KM
+        {
+            get { return this.fuelConsumptionPer1KM; }
+            set { this.fuelConsumptionPer1KM = value; }
+        }
+
+        public double TraveledDistance
+        {
+            get { return this.traveledDistance; }
+            set { this.traveledDistance = value; }
+        }
+
+        //Methods
+
+        public void isTravelPossible(double kmToTravel)
+        {
+            if (this.fuelAmmount < this.fuelConsumptionPer1KM * kmToTravel)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.fuelAmmount -= this.fuelConsumptionPer1KM * kmToTravel;
+                this.traveledDistance += kmToTravel;
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced/Exams/DefiningClasses/SpeedRacing/SpeedRacing/SpeedRacing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    class SpeedRacing
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carPool = new List<Car>();
+            int inputN = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputN; i++)
+            {
+                string[] inputLines = Console.ReadLine().Split(" ");
+                string model = inputLines[0];
+                double fuelAmmount = double.Parse(inputLines[1]);
+                double fuelConsumptionPer1KM = double.Parse(inputLines[2]);
+
+                Car car = new Car(model, fuelAmmount, fuelConsumptionPer1KM);
+
+                carPool.Add(car);
+            }
+
+            string[] commands = Console.ReadLine().Split(" ");
+            while (commands[0] != "End")
+            {
+                string model = commands[1];
+                double kmToTravel = double.Parse(commands[2]);
+
+               carPool.Where(car => car.Model == model).ToList().ForEach(car => car.isTravelPossible(kmToTravel));
+               commands = Console.ReadLine().Split(" ");
+            }
+
+            foreach (var car in carPool)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmmount:f2} {car.TraveledDistance}");
+            }
+        }
+    }
+}
