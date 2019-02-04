bc396a7dc232545abaa29ff59a7a5e889825eb3c
Ivan Spiridonov <ivan.spiridonov@nhqsa.nato.int>
2019-02-04 09:10:26 +0100

    Forgotten

---
+++ C# Advanced/Lessons/InheritanceAndAbstractions/Shapes/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value < 4)
+                {
+                    throw new ArgumentException("Height must me 4 or more!");
+                }
+                height = value;
+            }
+        }
+
+
+        public int Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value < 4)
+                {
+                    throw new ArgumentException("Width must me 4 or more!");
+                }
+                width = value;
+            }
+        }
+
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new String('*', Width));
+
+            for (int i = 0; i < Height - 2; i++)
+            {
+                Console.WriteLine($"*{new string(' ', Width - 2)}*");
+            }
+
+            Console.WriteLine(new String('*', Width));
+        }
+    }
+}
---
+++ C# Advanced/Lessons/InheritanceAndAbstractions/Shapes/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Shapes
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var shape = new Rectangle(8,5);
+            shape.Draw();
+        }
+    }
+}
